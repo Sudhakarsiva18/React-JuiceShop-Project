60a1c90d7d6c0b1bbf4f91f5ee9b00da26cbd463
Sudhakarsiva18 <108916787+Sudhakarsiva18@users.noreply.github.com>
2023-04-05 15:13:19 +0530

    Add files via upload

---
+++ Components/SidebarData.js
@@ -0,0 +1,27 @@
+export const SidebarData = [
+    {
+        title:"Home",
+        path:"/",
+        cName: "nav-text",
+    },
+    {
+        title:"Juices",
+        path:"/juices",
+        cName: "nav-text",
+    },
+    {
+        title:"Milkshakes",
+        path:"/milkshakes",
+        cName: "nav-text",
+    },
+    {
+        title:"Mojito",
+        path:"/mojito",
+        cName: "nav-text",
+    },
+    {
+        title:"Contact",
+        path:"/contact",
+        cName: "nav-text",
+    }
+]
